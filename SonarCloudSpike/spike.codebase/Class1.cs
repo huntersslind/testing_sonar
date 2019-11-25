ec02d30c6aab1b42f5685f358c7a5e6c5a83ee20
Hunter Lind <hunter.s.lind@accenture.com>
2019-11-25 16:02:38 -0600

    this should not work

--- SonarCloudSpike/spike.codebase/Class1.cs
+++ SonarCloudSpike/spike.codebase/Class1.cs
@@ -1,7 +1,39 @@
-﻿namespace spike.codebase
+﻿using System;
+
+namespace spike.codebase
 {
     public class Class1
     {
-        
+        public string fizz(int input)
+        {
+            if (input == 0)
+            {
+                return "0";
+            }
+            else if (input == 1)
+            {
+                return "1";
+            }
+            else if (input == 2)
+            {
+                return "2";
+            }
+            else if (input == 3)
+            {
+                return "fizz";
+            }
+            else if (input == 4)
+            {
+                return "4";
+            }
+            else if (input == 5)
+            {
+                return "buzz";
+            }
+            else if (input == 6)
+            {
+                return "6";
+            }
+        }
     }
 }
