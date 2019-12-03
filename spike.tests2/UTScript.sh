554734b776e21f26e052f10eafe075c3f0dd42f8
Hunter Lind <hunter.s.lind@accenture.com>
2019-12-03 12:06:32 -0600

    doot doot

--- spike.tests2/UTScript.sh
+++ spike.tests2/UTScript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # cd SonarCloudSpike/spike.tests2
 mkdir TestResults
-dotnet test  -p:CollectCoverage=true  -p:CoverletOutputFormat=opencover -p:CoverletOutput=TestResults/results.xml
+dotnet test  -p:CollectCoverage=true  -p:CoverletOutputFormat=opencover -p:CoverletOutput=../TestResults/opencover.xml --logger 'trx;LogFileName=ci_tests.trx'
