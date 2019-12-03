8dbd7e2b23880d8cad37ed4c8da1b6df2b50e201
Hunter Lind <hunter.s.lind@accenture.com>
2019-12-03 11:38:17 -0600

    quoted path

--- SonarCloudSpike/spike.tests2/UTScript.sh
+++ SonarCloudSpike/spike.tests2/UTScript.sh
@@ -1,4 +1,3 @@
 # cd SonarCloudSpike/spike.tests2
 mkdir TestResults
-dotnet add coverlet
-dotnet test SonarCloudSpike //p:CollectCoverage=true  //p:CoverletOutputFormat=opencover //p:CoverletOutput=../TestResults/results.trx
+dotnet test SonarCloudSpike /p:CollectCoverage=true  /p:CoverletOutputFormat=opencover /p:CoverletOutput='../TestResults/results.trx'
