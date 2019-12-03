cfe30314001ffc58a173a268ac92fed20f0bc197
Hunter Lind <hunter.s.lind@accenture.com>
2019-12-03 11:51:25 -0600

    tests

--- spike.tests2/UTScript.sh
+++ spike.tests2/UTScript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # cd SonarCloudSpike/spike.tests2
 mkdir TestResults
-dotnet test SonarCloudSpike //p:CollectCoverage=true  //p:CoverletOutputFormat=opencover //p:CoverletOutput='../TestResults/results.trx'
+dotnet test  -p:CollectCoverage=true  -p:CoverletOutputFormat=opencover -p:CoverletOutput=TestResults/results.trx
