54fd097f34622f65d3042ae70aa2972a167b80d1
Hunter Lind <hunter.s.lind@accenture.com>
2019-12-03 12:25:32 -0600

    testing new one

--- spike.tests2/UTScript.sh
+++ spike.tests2/UTScript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # cd SonarCloudSpike/spike.tests2
 mkdir TestResults
-dotnet test  -p:CollectCoverage=true  -p:CoverletOutputFormat=opencover -p:CoverletOutput=../TestResults/opencover.xml --logger 'trx;LogFileName=ci_tests.trx'
+dotnet test --logger trx --no-build -p:CollectCoverage=true -p:CoverletOutputFormat=opencover -p:CoverletOutput=coverage/
+# dotnet test  -p:CollectCoverage=true  -p:CoverletOutputFormat=opencover -p:CoverletOutput=../TestResults/opencover.xml --logger 'trx;LogFileName=ci_tests.trx' 
